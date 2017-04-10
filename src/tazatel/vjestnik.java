bf6d5fb7e7a368cad442339d9959f684627b0ef2
vojta3310 <vojta3310@seznam.cz>
2017-04-10 16:59:12 +0200

    vybera další nahodnou

--- src/tazatel/vjestnik.java
+++ src/tazatel/vjestnik.java
@@ -7,6 +7,8 @@ package tazatel;
 
 import AbstractList.AbstractList;
 import AbstractList.IAbstractList;
+import AbstractList.IIterator;
+import java.util.Random;
 
 /**
  *
@@ -27,7 +29,41 @@ public class vjestnik {
   }
   
   public void next(){  //vygeneruje dalsi nahodnou
+    int i=0;
+    int c=Integer.MAX_VALUE;
+    IIterator a=otazky.vytvorIterator();
+    a.vynulujIterator();
+    otazka b=(otazka) a.zpristupniAktualni();
+    while (a.hasNext()){  //zjistim nejniší c
+      if (b.getUsed()<c)
+        c=b.getUsed();
+      b=(otazka) a.getNext();
+    }
     
+    AbstractList<otazka> d;
+    d=new AbstractList();
+    a.vynulujIterator();
+    b=(otazka) a.zpristupniAktualni();
+    while (a.hasNext()){  //vsechny s nejmensim vyberu
+      if (b.getUsed()==c){
+        i++;
+        d.add(b);
+      }
+        c=b.getUsed();
+      b=(otazka) a.getNext();
+    }
+    
+    //vybrat náhodnou z d
+    Random randomGenerator= new Random();
+    int e=randomGenerator.nextInt(i);
+    IIterator f =d.vytvorIterator();
+    f.vynulujIterator();
+    otazka g=(otazka) f.zpristupniAktualni();
+    for (int j = 0; j < i; j++) {
+      g=(otazka) f.getNext();
+    }
+    aktualni=g;
+    hotovo=otazky.getPocet()-i;
   }
 
   public otazka getAktualni() { //vraci aktualni
